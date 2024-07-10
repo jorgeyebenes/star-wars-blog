467520285c2dc5c5ceaf6661b928db4ffc917120
jorgeyebenes <159541179+jorgeyebenes@users.noreply.github.com>
2024-07-10 01:48:20 +0000

    latest commit

--- src/js/layout.js
+++ src/js/layout.js
@@ -14,10 +14,9 @@ import { InfoPersonaje } from "./views/infoPersonaje";
 import { InfoPlaneta } from "./views/infoPlanetas";
 import { InfoVehiculo } from "./views/infoVehiculos";
 
-//create your first component
+
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	
 	const basename = process.env.BASENAME || "";
 
 	return (
